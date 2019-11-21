b5f6be0791d04b94ecdc70b6f0a30f0415c15daf
Chris Chan <eahk@users.noreply.github.com>
2019-11-21 12:38:02 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,43 @@
 <template>
-	<main id="app" class="main">
-		<el-container>
-			<el-header class="header">
-				<header class="header">
-					<div class="container">
-						<Navbar :routers="routers"></Navbar>
-					</div>
-				</header>
-			</el-header>
-			<!-- header -->
-			<el-main>
-				<el-container>
-					<el-row style="width: 100%;">
-						<el-col :md="6" class="hidden-sm-and-down">
-							<Sidebar :routers="routers"></Sidebar>
-						</el-col>
-						<el-col :md="18">
-							<MainContent></MainContent>
-						</el-col>
-					</el-row>
-				</el-container>
-			</el-main>
-			<!-- main -->
-			<el-footer class="footer" height="auto">
-				<AppFooter />
-			</el-footer>
-			<!-- footer -->
-			<el-button v-if="fixBtnVisible" type="danger" icon="el-icon-edit" class="hidden-md-and-up fix-btn" circle @click="directTo('applyForm')"></el-button>
-		</el-container>
-	</main>
+  <main id="app" class="main">
+    <el-container>
+      <el-header class="header">
+        <header class="header">
+          <div class="container">
+            <Navbar :routers="routers"></Navbar>
+          </div>
+        </header>
+      </el-header>
+      <!-- header -->
+      <el-main>
+        <el-container>
+          <el-row style="width: 100%;">
+            <el-col :md="6" class="hidden-sm-and-down">
+              <Sidebar :routers="routers"></Sidebar>
+            </el-col>
+            <el-col :md="18">
+              <MainContent></MainContent>
+            </el-col>
+          </el-row>
+        </el-container>
+      </el-main>
+      <!-- main -->
+      <el-footer class="footer" height="auto">
+        <el-container>
+          <AppFooter />
+        </el-container>
+      </el-footer>
+      <!-- footer -->
+      <el-button
+        v-if="fixBtnVisible"
+        type="danger"
+        icon="el-icon-edit"
+        class="hidden-md-and-up fix-btn"
+        circle
+        @click="directTo('applyForm')"
+      ></el-button>
+    </el-container>
+  </main>
 </template>
 
 <script>
@@ -38,125 +47,132 @@ import Sidebar from "./views/Sidebar.vue";
 import AppFooter from "./views/AppFooter.vue";
 
 export default {
-	name: "app",
-	data() {
-		return {
-			fixBtnVisible: true,
-			routers: [
-				{
-					class: "",
-					icon: "el-icon-s-home",
-					link: "home",
-					label: "首頁"
-				},
-				{
-					class: "",
-					icon: "el-icon-aim",
-					link: "mission",
-					label: "組織使命"
-				},
-				{
-					class: "",
-					icon: "el-icon-user-solid",
-					link: "recruit",
-					label: "職務資訊"
-				},
-				{
-					class: "",
-					icon: "el-icon-postcard",
-					link: "personnel",
-					label: "升遷制度"
-				},
-				{
-					class: "",
-					icon: "el-icon-s-check",
-					link: "fundraiser",
-					label: "認識專員"
-				},
-				{
-					class: "",
-					icon: "el-icon-info",
-					link: "recruitInfo",
-					label: "聯絡方式"
-				},
-				{
-					class: "apply-btn",
-					icon: "el-icon-document-checked",
-					link: "applyForm",
-					label: "立即申請"
-				}
-			]
-		};
-	},
-	components: {
-		MainContent,
-		Sidebar,
-		Navbar,
-		AppFooter
-	},
-	created() {
-		if (this.$route.params.section == "applyForm") {
-			this.fixBtnVisible = false;
-		} else {
-			this.fixBtnVisible = true;
-		}
-	},
-	methods: {
-		directTo(link) {
-			if (link !== this.$route.params.section) {
-				this.$router.push({ path: link });
-			}
-			window.scrollTo(0, 0);
-		}
-	},
-	watch: {
-		"$route.params.section": function() {
-			if (this.$route.params.section == "applyForm") {
-				this.fixBtnVisible = false;
-			} else {
-				this.fixBtnVisible = true;
-			}
-		}
-	}
+  name: "app",
+  data() {
+    return {
+      fixBtnVisible: true,
+      routers: [
+        {
+          class: "",
+          icon: "el-icon-s-home",
+          link: "home",
+          label: "首頁"
+        },
+        {
+          class: "",
+          icon: "el-icon-aim",
+          link: "mission",
+          label: "組織使命"
+        },
+        {
+          class: "",
+          icon: "el-icon-user-solid",
+          link: "recruit",
+          label: "職務資訊"
+        },
+        {
+          class: "",
+          icon: "el-icon-postcard",
+          link: "personnel",
+          label: "升遷制度"
+        },
+        {
+          class: "",
+          icon: "el-icon-s-check",
+          link: "fundraiser",
+          label: "認識專員"
+        },
+        {
+          class: "",
+          icon: "el-icon-info",
+          link: "recruitInfo",
+          label: "聯絡方式"
+        },
+        {
+          class: "apply-btn",
+          icon: "el-icon-document-checked",
+          link: "applyForm",
+          label: "立即申請"
+        }
+      ]
+    };
+  },
+  components: {
+    MainContent,
+    Sidebar,
+    Navbar,
+    AppFooter
+  },
+  created() {
+    if (this.$route.params.section == "applyForm") {
+      this.fixBtnVisible = false;
+    } else {
+      this.fixBtnVisible = true;
+    }
+  },
+  methods: {
+    directTo(link) {
+      if (link !== this.$route.params.section) {
+        this.$router.push({ path: link });
+      }
+      window.scrollTo(0, 0);
+    }
+  },
+  watch: {
+    "$route.params.section": function() {
+      if (this.$route.params.section == "applyForm") {
+        this.fixBtnVisible = false;
+      } else {
+        this.fixBtnVisible = true;
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .main {
-	>.el-container {
-		min-height: 100vh;
-	}
-	.el-header {
-		background-color: #6c0;
-		box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.4);
-		color: white;
-		.header {
-			.container {
-				max-width: 1200px;
-				margin-left: auto;
-				margin-right: auto;
-			}
-		}
-	}
-	.el-main {
-		flex: 1;
-		.el-container {
-			max-width: 1200px;
-			margin-left: auto;
-			margin-right: auto;
-		}
-	}
-	.el-footer {
-		height: auto;
-		margin-top: 60px;
-		background-color: #292f47;
-	}
-	.fix-btn {
-		z-index: 1200;
-		position: fixed;
-		bottom: 16px;
-		right: 16px;
-		font-size: 2rem !important;
-	}
+  > .el-container {
+    min-height: 100vh;
+  }
+  .el-header {
+    background-color: #6c0;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.4);
+    color: white;
+    .header {
+      .container {
+        max-width: 1440px;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    }
+  }
+  .el-main {
+    flex: 1;
+    .el-container {
+      max-width: 1440px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  .footer {
+    height: auto;
+    margin-top: 60px;
+    background-color: #292f47;
+    font-size: 14px;
+    padding: 2rem 0;
+    .el-container {
+      max-width: 1440px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  .fix-btn {
+    z-index: 1200;
+    position: fixed;
+    bottom: 16px;
+    right: 16px;
+    font-size: 2rem !important;
+  }
 }
 </style>
--- src/views/AppFooter.vue
+++ src/views/AppFooter.vue
@@ -1,55 +1,43 @@
 <template>
-  <footer class="footer">
-    <div class="container">
-      <div class="footer-upper">
-        <div class="footer__logo">
-          <img src="@/assets/img/GP-logo-2019-white-[web].png" alt="綠色和平" />
-        </div>
-        <div class="footer__text">
-          <div class="footer__note">
-            <p>
-              綠色和平存在，因為脆弱的地球需要改變、需要行動。
-              <br />但保護地球的使命不能僅靠綠色和平來完成，唯有結合你的力量，才能夠讓地球變得更綠色、更和平。
-            </p>
-          </div>
-        </div>
-        <div class="footer__links">
-          <ul>
-            <li>
-              <a href="http://www.greenpeace.org/hk/">首頁</a>
-            </li>
-            <li>
-              <a href="https://act.greenpeace.org/page/4663/donate/1">捐助支持</a>
-            </li>
-            <li>
-              <a
-                href="http://www.greenpeace.org/hk/about/privacy/"
-                title="私隱政策與個人資料收集聲明"
-              >私隱政策與個人資料收集聲明</a>
-            </li>
-          </ul>
+  <div style="width: 100%; padding: 0 16px;">
+    <div class="footer-upper">
+      <div class="footer__logo">
+        <img src="@/assets/img/GP-logo-2019-white-[web].png" alt="綠色和平" />
+      </div>
+      <div class="footer__text">
+        <div class="footer__note">
+          <p>
+            綠色和平存在，因為脆弱的地球需要改變、需要行動。
+            <br />但保護地球的使命不能僅靠綠色和平來完成，唯有結合你的力量，才能夠讓地球變得更綠色、更和平。
+          </p>
         </div>
       </div>
-      <div class="footer-lower">
-        <div class="footer__credit">
-          <p>© GREENPEACE 2019</p>
-        </div>
+      <div class="footer__links">
+        <ul>
+          <li>
+            <a href="http://www.greenpeace.org/hk/">首頁</a>
+          </li>
+          <li>
+            <a href="https://act.greenpeace.org/page/4663/donate/1">捐助支持</a>
+          </li>
+          <li>
+            <a
+              href="http://www.greenpeace.org/hk/about/privacy/"
+              title="私隱政策與個人資料收集聲明"
+            >私隱政策與個人資料收集聲明</a>
+          </li>
+        </ul>
       </div>
     </div>
-  </footer>
+    <div class="footer-lower">
+      <div class="footer__credit">
+        <p>© GREENPEACE 2019</p>
+      </div>
+    </div>
+  </div>
 </template>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.footer {
-  background-color: #292f47;
-  font-size: 14px;
-  padding: 2rem 0;
-  .container {
-    max-width: 1200px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-}
+<style lang="scss">
 .footer-upper {
   display: grid;
   padding: 2rem 0;
